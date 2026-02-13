39a8bd014ac18f79560dac5d0c3991a0150255f5
xretic <flourchik@gmail.com>
2026-02-13 10:32:04 +0100

    feat: add dev script

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected title = 'web';
+  protected title = 'Welcome';
 }
