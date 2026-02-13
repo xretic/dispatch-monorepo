65e89f06117fb5dd41e53d99ca71f2f470dc52c7
xretic <flourchik@gmail.com>
2026-02-13 12:21:32 +0100

    feat: add header chores: prettier config

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,18 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Header } from './layout/header/header';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, Header],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   count = signal(0);
+  protected description = 'test';
 
   increment() {
     this.count.set(this.count() + 1);
   }
-
-  protected description = 'test';
 }
