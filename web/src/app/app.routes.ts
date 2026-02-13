088d88fdf19a531b90effb93772eec15cfe832df
2Revol2 <maksimbaluta02@gmail.com>
2026-02-13 12:43:01 +0100

    feat: add home and dashboard, app routes

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then(m => m.Home)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
+  },
+];
--- web/src/app/layout/header/header.ts
+++ web/src/app/layout/header/header.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+      RouterLink
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
