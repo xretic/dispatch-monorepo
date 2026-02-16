93ef1c5ec3695fc2ebcda148d3b5ec6f0faeaac3
2Revol2 <maksimbaluta02@gmail.com>
2026-02-16 14:45:33 +0100

    feat: update header based on authentication state

--- web/src/app/features/auth/api/auth.api.ts
+++ web/src/app/features/auth/api/auth.api.ts
@@ -1,26 +1,37 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, tap } from 'rxjs';
 
 export type User = { id: string; email: string; username: string; avatarUrl: string };
 
 @Injectable({ providedIn: 'root' })
 export class AuthApi {
     private http = inject(HttpClient);
-    private base = 'http://localhost:3000/api';
+    private base = 'http://localhost:3200/api';
+    private _user$ = new BehaviorSubject<User | null>(null);
+    user$ = this._user$.asObservable();
 
     register(data: { username: string; email: string; password: string }) {
-        return this.http.post<User>(`${this.base}/auth/register`, data);
+        return this.http
+            .post<User>(`${this.base}/auth/register`, data)
+            .pipe(tap((user) => this._user$.next(user)));
     }
 
     login(data: { email: string; password: string }) {
-        return this.http.post<{ user: User }>(`${this.base}/auth/login`, data);
+        return this.http
+            .post<{ user: User }>(`${this.base}/auth/login`, data)
+            .pipe(tap((res) => this._user$.next(res.user)));
     }
 
     me() {
-        return this.http.get<User | null>(`${this.base}/auth/me`);
+        this.http
+            .get<User | null>(`${this.base}/auth/me`)
+            .subscribe((user) => this._user$.next(user));
     }
 
     logout() {
-        return this.http.post(`${this.base}/auth/logout`, {});
+        return this.http
+            .post(`${this.base}/auth/logout`, {})
+            .pipe(tap(() => this._user$.next(null)));
     }
 }
--- web/src/app/layout/header/header.ts
+++ web/src/app/layout/header/header.ts
@@ -3,16 +3,25 @@ import { ThemeService } from '../../core/theme.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { AuthApi } from '../../features/auth/api/auth.api';
 
 @Component({
     selector: 'app-header',
-    imports: [RouterLink, MatIconModule, MatButtonModule, RouterLinkActive],
+    imports: [RouterLink, MatIconModule, MatButtonModule, RouterLinkActive, AsyncPipe],
     templateUrl: './header.html',
     styleUrl: './header.css',
 })
 export class Header {
     theme = inject(ThemeService);
-
     @Output() menuClick = new EventEmitter<void>();
     @Input() isMobile = false;
+    private auth = inject(AuthApi);
+
+    user$ = this.auth.user$;
+
+    async onLogout() {
+        await firstValueFrom(this.auth.logout());
+    }
 }
